97cd637f95256c82664d8e2822f2ceb32add94ab
Nick Holland <nkh@bluetel.co.uk>
2018-03-11 01:41:16 +0000

    added function for rendering template

--- web/front.php
+++ web/front.php
@@ -6,6 +6,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
+
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
