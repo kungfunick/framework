3f921e3fe091a7f7c8576e4b01843dea50148e39
Nick Holland <nkh@bluetel.co.uk>
2018-03-11 01:33:53 +0000

    changed the routing to use matcher

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,3 +1,2 @@
-<?php
-// framework/src/pages/hello.php
-Hello <?php echo htmlspecialchars(isset($name) ? $name : 'World', ENT_QUOTES, 'UTF-8') ?>
+<!-- framework/src/pages/hello.php -->
+Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8');?>
